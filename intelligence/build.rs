bc70ea53a7e00a36fabc9de80b7fd6af86361d01
higrub89 <higuitaruben@hotmail.com>
2026-03-29 20:06:14 +0200

    fix(ci): replace deprecated tonic_build compile with compile_protos

--- intelligence/build.rs
+++ intelligence/build.rs
@@ -2,6 +2,6 @@ fn main() {
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
-        .compile(&["../core/proto/signal.proto"], &["../core/proto/"])
+        .compile_protos(&["../core/proto/signal.proto"], &["../core/proto/"])
         .unwrap();
 }
